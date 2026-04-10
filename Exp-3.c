94770373fcb8f6db2137e0543a38ba0c6a43b590
192521353simats-tech <192521353.simats@saveetha.com>
2026-04-10 08:47:12 +0530

    Update Exp-3.c

--- Exp-3.c
+++ Exp-3.c
@@ -3,30 +3,31 @@ int main() {
     int n, i;
     printf("Enter number of processes: ");
     scanf("%d", &n);
-    int at[n], bt[n], ct[n], tat[n], wt[n];
-    float avg_wt = 0, avg_tat = 0;
 
-    for(i = 0; i < n; i++) 
-    {
-        printf("P%d Arrival & Burst: ", i + 1);
-        scanf("%d %d", &at[i], &bt[i]);
+    int bt[n], ct[n], tat[n], wt[n];
+    float total_wt = 0, total_tat = 0;
+
+    for(i = 0; i < n; i++) {
+        printf("P%d Burst Time: ", i + 1);
+        scanf("%d", &bt[i]);
     }
-    for(i = 0; i < n; i++)
-    {
-        if (i == 0 || at[i] > ct[i-1]) 
-            ct[i] = at[i] + bt[i];
-        else 
-            ct[i] = ct[i-1] + bt[i];
-
-        tat[i] = ct[i] - at[i];
+    int current_time = 0;
+    for(i = 0; i < n; i++) {
+        current_time += bt[i];
+        ct[i] = current_time;
+        
+        tat[i] = ct[i];      
         wt[i] = tat[i] - bt[i];
         
-        avg_tat += tat[i];
-        avg_wt += wt[i];
+        total_tat += tat[i];
+        total_wt += wt[i];
     }
-    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+
+    printf("\nPID\tBT\tCT\tTAT\tWT\n");
     for(i = 0; i < n; i++)
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
-    printf("\nAvg Waiting Time: %.2f\n", avg_wt/n);
+        printf("%d\t%d\t%d\t%d\t%d\n", i+1, bt[i], ct[i], tat[i], wt[i]);
+    printf("\nAverage Waiting Time: %.2f", total_wt/n);
+    printf("\nAverage Turnaround Time: %.2f\n", total_tat/n);
+
     return 0;
 }
